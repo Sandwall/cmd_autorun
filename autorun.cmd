caf6858efc837698e6509d1c918da6a976995cf1
Sandwall <17184361+Sandwall@users.noreply.github.com>
2025-03-16 14:38:43 -0400

    real initial (oops)
    small change to README

--- autorun.cmd
+++ autorun.cmd
@@ -3,7 +3,6 @@
 REM $M is a remote name for network drives
 REM $_ is a carriage return/newline
 REM $T is the current time
-REM $S is a space character
 REM $P is current drive/directory path
 REM $G is the > character
 REM $E[32m sets color to green, $E[0m resets color
